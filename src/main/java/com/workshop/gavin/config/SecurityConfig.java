11b1f53a964000d7d55da59d8e42bee3e2b18b33
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-20 12:54:40 +0000

    Added a SecurityConfig class with a basic securityFilterChain Bean.

---
+++ src/main/java/com/workshop/gavin/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.workshop.gavin.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return httpSecurity.build();
+    }
+
+}
